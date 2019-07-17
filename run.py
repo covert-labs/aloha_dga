40b8675ed6c4055deb13b54d3f5b3b3744f741ec
Jason Trost <jason.trost@gmail.com>
2019-07-17 19:30:06 -0400

    run.py outputs ALL THE ROCs now

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ RESULT_FILE = 'results.pkl'
 def run_experiments(nfolds=10):
 
     options = {
-        'nfolds': nfolds, 
+        'nfolds': nfolds,
         # enable for quick functional testing
         # 'max_epoch':2
     }
@@ -106,39 +106,74 @@ def create_figs(nfolds=10, force=False):
             fpr, tpr, auc = calculate_metrics(model_result)
             metrics.append({
                 'name': name,
-                'fpr': fpr, 
-                'tpr': tpr, 
+                'fpr': fpr,
+                'tpr': tpr,
                 'auc': auc,
             })
 
+    colors = {
+        'lstm': 'green',
+        'cnn': 'red',
+        'bigram': 'blue',
+        'cnn_lstm': 'orange',
+    }
+
+    # these control which models get grouped together when ROC images get created.
+    metrics_filters = {
+        'all': ('lstm', 'aloha_lstm','cnn', 'aloha_cnn','cnn_lstm', 'aloha_cnn_lstm','bigram', 'aloha_bigram',),
+        'lstm': ('lstm', 'aloha_lstm',),
+        'cnn': ('cnn', 'aloha_cnn',),
+        'cnn_lstm': ('cnn_lstm', 'aloha_cnn_lstm',),
+        'bigram': ('bigram', 'aloha_bigram',),
+    }
+
     # Save figures
-    from matplotlib import pyplot as plt
-    with plt.style.context('bmh'):
-        for metrics_record in sorted(metrics, key=lambda rec: rec['auc'], reverse=True):
-            plt.plot(
-                metrics_record['fpr'], 
-                metrics_record['tpr'], 
-                label='%s (AUC = %.4f)' % (metrics_record['name'].upper(), metrics_record['auc'], ), 
-                rasterized=True
-            )
+    from matplotlib import pyplot
+    with pyplot.style.context('bmh'):
+        for plot_name, metrics_filter in metrics_filters.items():
+            fig1, plt = pyplot.subplots()
 
-        plt.ylim([0.0, 1.05])
-        plt.xlabel('False Positive Rate', fontsize=18)
-        plt.ylabel('True Positive Rate', fontsize=18)
-        plt.title('ROC - Binary Classification', fontsize=26)
-        plt.legend(loc="lower right", fontsize=10)
-        plt.tick_params(axis='both', labelsize=18)
+            metrics_to_plot = [rec for rec in metrics if rec['name'] in metrics_filter]
+            for metrics_record in sorted(metrics_to_plot, key=lambda rec: rec['auc'], reverse=True):
 
-        # create ROC curves at various zooms at linear scale
-        for xmax in [0.2, 0.4, 0.6, 0.8, 1.0]:
-            plt.xlim([0.0, xmax])
-            plt.savefig('results-linear-{xmax}.png'.format(xmax=xmax), pad_inches=0.25, bbox_inches='tight')
+                linestyle = '-'
+                if 'aloha' in metrics_record['name']:
+                    linestyle = ':'
 
-        # create ROC curves at various zooms at log scale
-        plt.xscale('log')
-        for xmax in [0.5, 0.2, 0.1, 0.01, 0.001, 0.0001]:
-            plt.xlim([0.0, xmax])
-            plt.savefig('results-logscale-{xmax}.png'.format(xmax=xmax), pad_inches=0.25, bbox_inches='tight')
+                color = colors[metrics_record['name'].replace('aloha_', '')]
+
+                plt.plot(
+                    metrics_record['fpr'],
+                    metrics_record['tpr'],
+                    label='%s (AUC = %.4f)' % (metrics_record['name'].upper(), metrics_record['auc'], ),
+                    rasterized=True,
+                    linestyle=linestyle,
+                    color=color,
+
+                )
+
+            plt.set_ylim([0.0, 1.05])
+            plt.set_xlabel('False Positive Rate', fontsize=16)
+            plt.set_ylabel('True Positive Rate', fontsize=16)
+            plt.set_title('ROC - Binary Classification (%s)'%(metrics_record['name'].upper(),), fontsize=20)
+            plt.legend(loc="best", fontsize=9)
+            plt.tick_params(axis='both', labelsize=16)
+
+            # create ROC curves at various zooms at linear scale
+            plt.set_xscale('linear')
+            for xmax in [0.2, 0.4, 0.6, 0.8, 1.0]:
+                plt.set_xlim([0.0, xmax])
+                fig1.savefig('results-linear-{plot_name}-{xmax}.png'.format(xmax=xmax, plot_name=plot_name), pad_inches=0.25, bbox_inches='tight')
+
+            # create ROC curves at various zooms at log scale
+            plt.set_xscale('log')
+            for xmax in [0.5, 0.2, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001]:
+                plt.set_xlim([0.0, xmax])
+                fig1.savefig('results-logscale-{plot_name}-{xmax}.png'.format(xmax=xmax, plot_name=plot_name), pad_inches=0.25, bbox_inches='tight')
+
+            for xmax in [1.05]:
+                plt.set_xlim([0.000001, xmax])
+                fig1.savefig('results-logscale-{plot_name}-0.000001-to-{xmax}.png'.format(xmax=xmax, plot_name=plot_name), pad_inches=0.25, bbox_inches='tight')
 
 
 if __name__ == "__main__":
